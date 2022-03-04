75c0f0f40cfed0959dedda389b7b3361bf42d58b
dh711 <dhairya711@gmail.com>
2022-03-04 15:33:44 +0400

    update /logout.

--- loginServices/controllers/logoutController.js
+++ loginServices/controllers/logoutController.js
@@ -2,33 +2,29 @@ const db = require("../config/db");
 const jwt = require("jsonwebtoken");
 
 const logout = (req, res) => {
-    // Access is denied if refresh token is not sent.
-    if (!req.cookies.refreshToken) res.status(401).send("Access denied.");
+    // Access is denied if access token is not sent.
+    if (!req.signedCookies.accessToken) res.status(401).send("Access denied.");
     else {
         try {
-            // Verify token if it exists.
+            // Validate token if it exists.
             const tokenPayload = jwt.verify(
-                req.cookies.refreshToken,
-                process.env.JWT_REFRESH_SECRET
+                req.signedCookies.accessToken,
+                process.env.JWT_SECRET
             );
 
-            // Remove token from database.
-            db.query(
-                `UPDATE ${tokenPayload.userType} SET refresh_token = NULL WHERE refresh_token = ?`,
-                [req.cookies.refreshToken],
-                (error) => {
-                    if (error) {
-                        res.status(500).send(error);
-                    } else {
-                        // Send JWT.
-                        res.cookie("refreshToken", {
-                            expires: Date.now(),
-                        }).send("Logout sucessfull");
-                    }
-                }
-            );
+            // Check if token belongs to user.
+            if (req.header("userID") !== tokenPayload.userID)
+                throw { name: "UnauthorizedUserError", error: new Error() };
+
+            // Remove cookie by expiry.
+            res.cookie("accessToken", {
+                expires: Date.now(),
+            }).json({
+                userID: "",
+                isLoggedIn: false,
+            });
         } catch (error) {
-            if (error.name === "JsonWebTokenError")
+            if (error.name === "JsonWebTokenError" || "UnauthorizedUserError")
                 res.status(401).send("Invalid token.");
             else if (error.name === "TokenExpiredError")
                 res.status(403).send("Token expired.");
--- loginServices/utils/authenticate.js
+++ loginServices/utils/authenticate.js
@@ -1,24 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
-    // Access is denied to resource if token does not exist.
+    // Access is denied if access token is not sent.
     if (!req.signedCookies.accessToken) res.status(401).send("Access denied.");
-    try {
-        // Verify token if it exists.
-        const tokenPayload = jwt.verify(
-            req.signedCookies.accessToken,
-            process.env.JWT_SECRET
-        );
+    else {
+        try {
+            // Validate token if it exists.
+            const tokenPayload = jwt.verify(
+                req.signedCookies.accessToken,
+                process.env.JWT_SECRET
+            );
 
-        if (req.header("userID") !== tokenPayload.userID)
-            throw { name: "UnauthorizedUserError", error: new Error() };
+            // Check if token belongs to user.
+            if (req.header("userID") !== tokenPayload.userID)
+                throw { name: "UnauthorizedUserError", error: new Error() };
 
-        next();
-    } catch (error) {
-        if (error.name === "JsonWebTokenError" || "UnauthorizedUserError")
-            res.status(401).send("Invalid token.");
-        else if (error.name === "TokenExpiredError")
-            res.status(403).send("Token expired.");
+            next();
+        } catch (error) {
+            if (error.name === "JsonWebTokenError" || "UnauthorizedUserError")
+                res.status(401).send("Invalid token.");
+            else if (error.name === "TokenExpiredError")
+                res.status(403).send("Token expired.");
+        }
     }
 };
 
