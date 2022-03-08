7f2ec42425f8c9a1b1e017a1334606b84c592cb6
dh711 <dhairya711@gmail.com>
2022-03-09 00:22:30 +0400

    cityMaster corrections.

--- cityServices/index.js
+++ cityServices/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const app = express();
-const mysql = require("mysql2");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mysql = require("mysql2");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const app = express();
 dotenv.config();
 
+// Connecting to database.
 const db = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
@@ -13,25 +17,8 @@ const db = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
-const jwt = require("jsonwebtoken");
 
-// const authenticate = (req, res, next) => {
-//     // Get token from request header.
-//     const token = req.headers['accesstoken'];
-//     // Access is denied to resource if token does not exist.
-//     if (!token) res.status(401).send("Access denied.");
-
-//     // Verify token if it exists.
-//     try {
-//         const verify = jwt.verify(token, process.env.JWT_SECRET);
-//         req.user = verify;
-//         req.isAuthenticated = true;
-//         next();
-//     } catch (error) {
-//         res.status(401).send("Invalid token.");
-//     }
-// };
-
+// Middlewares.
 app.use(
     cors({
         origin: "http://localhost:3000",
@@ -39,68 +26,63 @@ app.use(
     })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.json());
-// app.use(authenticate);
+
 app.listen(3001, () => {
     console.log("Run on 3001");
 });
 
-app.post("/cityMaster/Add", (req, res) => {
-    console.log("chal jaa be");
-    const sql = "INSERT INTO CITYMASTER VALUES(?,?);";
-    const cityname = req.body.City;
-    const statename = req.body.State;
-
-    db.query(sql, [cityname, statename], (err, result) => {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else {
-            res.send("1");
+app.post("/cityMaster/add", (req, res) => {
+    db.query(
+        "INSERT INTO CITYMASTER VALUES(?,?);",
+        [req.body.City, req.body.State],
+        (err) => {
+            if (err) {
+                console.log(err.sqlMessage);
+                res.send(err);
+            } else {
+                res.send("1");
+            }
         }
-    });
+    );
 });
 
-app.post("/cityMaster/Delete", (req, res) => {
-    const sql = "DELETE FROM CITYMASTER WHERE CityName=?;";
-    const cityname = req.body.City;
-
-    db.query(sql, cityname, (err, result) => {
-        if (err) {
-            console.log(err);
-            res.send(err);
+app.post("/cityMaster/delete", (req, res) => {
+    db.query(
+        "DELETE FROM CITYMASTER WHERE CityName=?;",
+        req.body.City,
+        (err) => {
+            if (err) {
+                console.log(err.sqlMessage);
+                res.send(err.sqlMessage);
+            } else res.send("Successfully deleted");
         }
-        res.send("Successfully deleted");
-    });
+    );
 });
 
-app.get("/cityMaster/getdata", (req, res) => {
-
-    const sql = "SELECT * FROM CITYMASTER;";
-
-    db.query(sql, (err, result) => {
+app.get("/cityMaster/get", (req, res) => {
+    db.query("SELECT * FROM CITYMASTER;", (err, result) => {
         if (err) {
-            console.log(err);
-            res.send(err);
+            console.log(err.sqlMessage);
+            res.send(err.sqlMessage);
         } else {
-            console.log(result);
             res.send(result);
         }
     });
 });
 
-app.post("/cityMaster/Update", (req, res) => {
-    const sql =
-        "UPDATE CITYMASTER SET CityName=?,StateName=? WHERE CityName=?;";
-    const cityname = req.body.City;
-    const statename = req.body.State;
-    const oldcity = req.body.oldcity;
-    db.query(sql, [cityname, statename, oldcity], (err, result) => {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else {
-            res.send("1");
+app.post("/cityMaster/update", (req, res) => {
+    db.query(
+        "UPDATE CITYMASTER SET CityName=?,StateName=? WHERE CityName=?;",
+        [req.body.City, req.body.State, req.body.oldcity],
+        (err) => {
+            if (err) {
+                console.log(err.sqlMessage);
+                res.send(err.sqlMessage);
+            } else {
+                res.send("1");
+            }
         }
-    });
+    );
 });
