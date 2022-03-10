8b58b972e09a48349da5f4f20f976d52bdd5e66d
rutvay <rutvaydhami2112@gmail.com>
2022-03-11 00:34:14 +0400

    Changes in table name

--- accountservice/routes/auth.js
+++ accountservice/routes/auth.js
@@ -8,7 +8,7 @@ const FetchAll = require("../controllers/FetchAll");
 router.post("/postdata", Accountdata); // adds data to account_master table
 router.post("/deletedata", Deleteaccountdata); // deletes data from the table with given account name
 router.post("/updatedata", updatedata); // update the data in the table
-router.post("/FetchAll", FetchAll); // update the data in the table
+router.get("/FetchAll", FetchAll); // update the data in the table
 
 
 // router.get("/test", verifyToken, (req, res) => {
