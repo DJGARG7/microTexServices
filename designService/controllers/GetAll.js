b118e934ac6ca4a75d412d2cfb72a8b5f6c94de0
dh711 <dhairya711@gmail.com>
2022-03-28 14:43:08 +0400

    add .env

--- designService/controllers/GetAll.js
+++ designService/controllers/GetAll.js
@@ -1,10 +1,13 @@
 const db = require("../config/db");
-const {sendResponse} = require('../../helper');
+const { sendResponse } = require("../../helper");
 
 const FetchAll = (req, res) => {
     console.log(req.body);
     const query = "SELECT * FROM DesignMaster;";
-    db.query(query, (err, result) => sendResponse(res, err, result));
+    db.query(query, (err, result) => {
+        if (err) res.send(err);
+        else res.send(result);
+    });
 };
 
 module.exports = FetchAll;
