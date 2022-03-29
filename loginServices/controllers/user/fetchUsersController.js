8faf447b27e2675df61a212cca66642b3cf196d0
dh711 <dhairya711@gmail.com>
2022-03-29 10:22:08 +0400

    bug fix for fetch users.

--- loginServices/controllers/user/fetchUsersController.js
+++ loginServices/controllers/user/fetchUsersController.js
@@ -1,10 +1,13 @@
 const db = require("../../config/db");
 
 const fetchUsers = (req, res) => {
-    db.query("SELECT uuid, user_id FROM Firm", (error, results) => {
-        if (error) res.status(400).send(`${error.sqlMessage}`);
-        else res.send(results);
-    });
+    db.query(
+        "SELECT uuid, user_id FROM Firm WHERE admin = 0",
+        (error, results) => {
+            if (error) res.status(400).send(`${error.sqlMessage}`);
+            else res.send(results);
+        }
+    );
 };
 
 module.exports = fetchUsers;
