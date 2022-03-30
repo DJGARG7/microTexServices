8e24445913ec74b6125a0e8206532bbdebfbab64
rutvay <rutvaydhami2112@gmail.com>
2022-03-30 22:00:55 +0400

    final commit abot greypurchase

--- userServices/models/greypurchase.sql
+++ userServices/models/greypurchase.sql
@@ -1,27 +1,48 @@
-CREATE TABLE `billdetails` (
-    `BillNo` INT(10),
-    `BillDate` DATE,
-    `accntnames` VARCHAR(100) NOT NULL,
-    `ChallanNo` INT(10),
-    `ChallanDate` DATE,
-    `Agent` VARCHAR(10),
-    `EntryNo` INT(10),
-    PRIMARY KEY (`BillNo`)
-);
+/* table that holds details regarding bill
+*/
 
+CREATE TABLE `billdetails` (
+ `BillNo` int(10) NOT NULL,
+ `BillDate` date DEFAULT NULL,
+ `accntnames` varchar(100) NOT NULL,
+ `ChallanNo` int(10) DEFAULT NULL,
+ `ChallanDate` date DEFAULT NULL,
+ `Agent` varchar(10) DEFAULT NULL,
+ `EntryNo` int(10) DEFAULT NULL,
+ PRIMARY KEY (`BillNo`)
+) 
+
+/*table that holds details regarding items
+*/
 
 CREATE TABLE `itemdetails` (
-    `BillNo` VARCHAR(10) NOT NULL,
-    `ItemName` VARCHAR(100),
-    `Marka` DECIMAL(20,2),
-    `Taka` INT(10),
-    `Mts` DECIMAL(20,2),
-    `Fold` INT(20),
-    `ActMts` DECIMAL(20,2),
-    `Rate` DECIMAL(20,2),
-    `Amount` DECIMAL(20,2),
-    `Discount` DECIMAL(20,2),
-    `IGST` DECIMAL(20,2),
-    `CGST` DECIMAL(20,2),
-    `SGST` DECIMAL(20,2)
-);
+ `BillNo` int(10) NOT NULL,
+ `ItemName` varchar(100) DEFAULT NULL,
+ `Marka` decimal(20,2) DEFAULT NULL,
+ `Taka` int(10) DEFAULT NULL,
+ `Mts` decimal(20,2) DEFAULT NULL,
+ `Fold` int(20) DEFAULT NULL,
+ `ActMts` decimal(20,2) DEFAULT NULL,
+ `Rate` decimal(20,2) DEFAULT NULL,
+ `Amount` decimal(20,2) DEFAULT NULL,
+ `Discount` decimal(20,2) DEFAULT NULL,
+ `IGST` decimal(20,2) DEFAULT NULL,
+ `CGST` decimal(20,2) DEFAULT NULL,
+ `SGST` decimal(20,2) DEFAULT NULL,
+ KEY `BillNo` (`BillNo`),
+ CONSTRAINT `test` FOREIGN KEY (`BillNo`) REFERENCES `billdetails` (`BillNo`) ON DELETE CASCADE ON UPDATE NO ACTION
+)
+
+/*table that holds different types of items
+*/
+CREATE TABLE `items` (
+ `itemname` varchar(20) NOT NULL,
+ `openingpcs` int(10) DEFAULT NULL,
+ `openingmts` int(10) DEFAULT NULL,
+ `openingval` int(10) DEFAULT NULL,
+ `rateperpcs` int(10) DEFAULT NULL,
+ `ratepermts` int(10) DEFAULT NULL,
+ `hsncode` int(10) DEFAULT NULL,
+ `gst` int(10) DEFAULT NULL,
+ `descriptiongst` varchar(100) DEFAULT NULL
+)
