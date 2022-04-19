26d8a9ee0fedec4359a8215bf2baecfca6f75b78
DJGARG7 <18bce052@nirmauni.ac.in>
2022-04-19 23:01:28 +0530

    sale transaction complete db schema will be updated in next commit

---
+++ userServices/controllers/sales/makeBill.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql2/promise");
+const config = require("../../config/transactionconnect");
+const Axios = require("axios");
+const makeBill = async (req, res) => {
+    const data = req.body;
+    console.log(data);
+    const connection = await mysql.createConnection(config);
+    await connection.execute("SET TRANSACTION ISOLATION LEVEL READ COMMITTED");
+    await connection.beginTransaction();
+    try {
+        await connection.execute(
+            "update sales_order set status=1 where CNAME=?",
+            [data.uid]
+        );
+        await connection.commit();
+        await Axios.post("http://localhost:3007/transaction/", data);
+        await connection.commit();
+        await connection.end();
+        res.send("1");
+    } catch (error) {
+        console.log("billing failed due to  ", error);
+        await connection.rollback();
+        await connection.end();
+        res.status(400).send(error);
+    }
+};
+module.exports = makeBill;
