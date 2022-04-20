bb974cb77279996038db743e9668a58b548afcc1
dh711 <dhairya711@gmail.com>
2022-04-21 00:05:48 +0400

    add support for taka endpoint.

--- userServices/controllers/purchases/fetchTaka.js
+++ userServices/controllers/purchases/fetchTaka.js
@@ -1,15 +1,14 @@
 const db = require("../../config/db");
 
 const getTaka = (req, res) => {
-    if (!req.params.itemID) {
+    if (!req.params.billNumber || !req.params.itemID) {
         res.status(400).send("Bad request.");
-    } else if (req.params.itemID === "DEFAULT") {
-        res.send([]);
+        // } else if (req.params.itemID === "DEFAULT") {
+        //     res.send([]);
     } else {
-        console.log("Taka");
         db.query(
-            "SELECT * FROM grey_takadetails WHERE itemID=?",
-            [req.params.itemID],
+            "SELECT * FROM GREY_TAKA_DETAILS WHERE billNumber = ? AND itemID=?",
+            [req.params.billNumber, req.params.itemID],
             (error, results) => {
                 if (error) {
                     res.status(400).send(`${error.sqlMessage}`);
