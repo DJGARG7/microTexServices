0e18ffe98dda719428fd1452cb50abd36753cb3e
DJGARG7 <18bce052@nirmauni.ac.in>
2022-04-22 12:05:03 +0530

    changed require to import in transaction service

--- transactionservice/config/db.js
+++ transactionservice/config/db.js
@@ -1,13 +1,16 @@
-const mysql = require("mysql2");
-
-// Creating connection pool.
-const connectionPool = mysql.createPool({
-    multipleStatements:true,
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+const config = {
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-});
+    multipleStatements: true,
+    port: 3306,
+};
+// Creating connection pool.
+const db = mysql.createPool(config);
 
-module.exports = connectionPool;
+export { db, config };
