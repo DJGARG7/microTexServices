8a43c2ce028f28fbc044ef54ac7995b24a2d1ffa
dh711 <dhairya711@gmail.com>
2022-04-29 01:45:10 +0400

    added endpoint to get grey purchase sum in a date range.

--- userServices/controllers/purchases/fetchItems.js
+++ userServices/controllers/purchases/fetchItems.js
@@ -1,7 +1,6 @@
 const db = require("../../config/db");
 
 const fetchItems = (req, res) => {
-    console.log("hello");
     db.query("SELECT * FROM grey_items;", (error, results) => {
         if (error) {
             res.status(500).send(`${error.sqlMessage}`);
--- userServices/controllers/purchases/getSum.js
+++ userServices/controllers/purchases/getSum.js
@@ -4,7 +4,14 @@ const getSum = (req, res) => {
     if (!req.params.startDate || !req.params.endDate)
         res.status(400).send("Bad request");
     else {
-        console.log("hello");
+        db.query(
+            "SELECT sum(billAmount) sum FROM grey_bills WHERE billDate >= ?; AND billDate <= ?;",
+            [req.params.startDate, req.params.endDate],
+            (error, results) => {
+                if (error) res.status(500).send(`${error.sqlMessage}`);
+                else res.send(results);
+            }
+        );
     }
 };
 
