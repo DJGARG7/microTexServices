d221f45dfcb2be5f3831de708de224e0f86eb156
dh711 <dhairya711@gmail.com>
2022-04-29 00:59:11 +0400

    updated schema for grey purchase and mill tables.

--- userServices/routes/purchaseRoute.js
+++ userServices/routes/purchaseRoute.js
@@ -3,26 +3,20 @@ const router = require("express").Router();
 // Imports for items.
 const addItem = require("../controllers/purchases/addItem");
 const fetchItems = require("../controllers/purchases/fetchItems");
+const addGreyBill = require("../controllers/purchases/addGreyBill");
+const fetchGreyBills = require("../controllers/purchases/fetchGreyBills");
 const fetchSuppliers = require("../controllers/purchases/fetchSuppliers");
+const fetchTaka = require("../controllers/purchases/fetchTaka");
 
 // Item and related routes.
 router.get("/items", fetchItems);
 router.post("/items", addItem);
-router.get("/suppliers/:itemID?", fetchSuppliers); // To get suppliers for an item from current inventory.
-
-// Imports for taka.
-const fetchTaka = require("../controllers/purchases/fetchTaka");
-
-// Routes for taka.
-router.get("/taka/:billNumber?/:itemID?", fetchTaka);
-
-// imports for purchase transactions
-const addGreyBill = require("../controllers/purchases/addGreyBill");
-const fetchGreyBills = require("../controllers/purchases/fetchGreyBills");
-
 // routes for purchase transactions
 router.get("/bills/:accountID?/:itemID?", fetchGreyBills);
 router.post("/bills", addGreyBill);
+router.get("/suppliers/:itemID?", fetchSuppliers); // To get suppliers for an item from current inventory.
+// Routes for taka.
+router.get("/taka/:billNumber?/:itemID?", fetchTaka);
 
 //imports for general purchase
 const fetchpurchase = require("../controllers/purchases/fetchgeneralpurchases");
