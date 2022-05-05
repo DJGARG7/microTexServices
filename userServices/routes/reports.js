564379c787e76960c4f7ddd5b27ed1968158549e
rutvay <rutvaydhami2112@gmail.com>
2022-05-06 03:28:12 +0400

    updated report

--- userServices/routes/reports.js
+++ userServices/routes/reports.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const balanceSheet = require("../controllers/reports/balanceSheet");
 // const receive = require("../controllers/cashBook/receive");
+const getTotalExpenses = require("../controllers/reports/generalReports/getTotalExpenses")
 
 // router.post("/payment", payment);
 router.get("/balancesheet", balanceSheet);
+router.get("/getExpense",getTotalExpenses);
 
 module.exports = router;
