fb5032ccc401604459c596e8db487e5fc4a184c2
DJGARG7 <18bce052@nirmauni.ac.in>
2022-05-17 01:41:07 +0530

    balance sheet get total getType in design master

--- userServices/routes/reports.js
+++ userServices/routes/reports.js
@@ -1,14 +1,15 @@
 const router = require("express").Router();
 
-const balanceSheet = require("../controllers/reports/balanceSheet");
-// const receive = require("../controllers/cashBook/receive");
-const getTotalExpenses = require("../controllers/reports/generalReports/getTotalExpenses")
+const balanceSheet = require("../controllers/reports/balanceSheet/balanceSheet");
+const totalBS = require("../controllers/reports/balanceSheet/totalBS");
+const getTotalExpenses = require("../controllers/reports/generalReports/getTotalExpenses");
 const getTotalItemSold = require("../controllers/reports/generalReports/getTotalItemSold");
 const getTotalAccountExpense = require("../controllers/reports/generalReports/getTotalAccountExpense");
-// router.post("/payment", payment);
+
 router.get("/balancesheet", balanceSheet);
-router.get("/getExpense",getTotalExpenses);
-router.get("/getTotalItemSold",getTotalItemSold);
-router.get("/getTotalAccountExpense",getTotalAccountExpense);
+router.get("/totalBS", totalBS);
+router.get("/getExpense", getTotalExpenses);
+router.get("/getTotalItemSold", getTotalItemSold);
+router.get("/getTotalAccountExpense", getTotalAccountExpense);
 
 module.exports = router;
